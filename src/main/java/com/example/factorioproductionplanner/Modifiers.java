9f84326ae1cd96d3e45bd8fcde0ffcfb2d78a01f
TheRandomFox <abdhalim123@hotmail.com>
2021-10-13 18:51:42 +0800

    Added classes
    MainActivity, Products, Modifiers

---
+++ src/main/java/com/example/factorioproductionplanner/Modifiers.java
@@ -0,0 +1,22 @@
+package com.example.factorioproductionplanner;
+
+public class Modifiers {
+    //attributes
+    //assembling machines
+    private double am1 = 2.0;
+    private double am2 = 1.33;
+    private double am3 = 0.8;
+
+    //furnaces
+    private double f_stone = 1.0;
+    private double f_steel = 0.5;
+    private double f_elec = 0.5;
+
+    //speed modules
+    private double speed1 = 1/1.2;
+    private double speed2 = 1/1.3;
+    private double speed3 = 1/1.5;
+
+    //methods
+    //nil
+}
