995830e81c9b5a22fa9262b8b7604cf8029c0468
TheRandomFox <abdhalim123@hotmail.com>
2021-10-13 18:59:34 +0800

    Renamed variables
    productName -> itemName

--- src/main/java/com/example/factorioproductionplanner/Products.java
+++ src/main/java/com/example/factorioproductionplanner/Products.java
@@ -2,7 +2,7 @@ package com.example.factorioproductionplanner;
 
 public class Products {
     //attributes
-    private String productName; //displayed name
+    private String itemName; //displayed name
     private int itemId;         //unique ID for referencing object
     private double baseTime;    //base crafting time in seconds
     private int noOfOutputs;    //number of output products per cycle
